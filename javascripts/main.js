20090cf4262901d3c7f39ff58accaa3127b53c2b
Ripal Patel <ripal212007@gmail.com>
2018-10-15 21:27:48 -0500

    loading fish

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,12 @@
 // Filter fish that are "on sale"
 
 // Add fish to "Basket"
+
+// load Fish
+$.get('../db/fishes.json')
+ .done((data)=>{
+     console.log(data);
+ })
+ .fail((error)=>{
+     console.error(error);
+ });
